dcbd48e1d29fb5969a0a80bcf7544fb9bceda002
AlexMRTY <asifrezai261@gmail.com>
2024-10-20 12:58:37 +0200

    fixed delete user

--- auctionWebApp/Program.cs
+++ auctionWebApp/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 // dependency injection of service into controller
 builder.Services.AddScoped<IAuctionItemService, AuctionItemService>();
@@ -21,7 +22,9 @@ builder.Services.AddScoped<IUserService, UserService>();
 
 // dependency injection of persistence into service
 builder.Services.AddScoped<IAuctionItemPersistence, AuctionItemPersistence>();
-builder.Services.AddScoped<IUserPersistence, UserPersistence>();
+
+// // builder.Services.AddScoped<UserManager<AppIdentityUser>>();
+
 
 // dependency injection of generic persistence into persistence
 builder.Services.AddScoped(typeof(IGenericPersistence<>), typeof(GenericPersistence<>));
@@ -35,6 +38,26 @@ builder.Services.AddDbContext<AuctionDbContext>(options =>
 builder.Services.AddDbContext<AppIdentityDbContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("IdentityDbConnection")));
 builder.Services.AddDefaultIdentity<AppIdentityUser>(options => options.SignIn.RequireConfirmedAccount = false).AddRoles<IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
 
+// builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+//     .AddEntityFrameworkStores<AppIdentityDbContext>()
+//     .AddDefaultTokenProviders();
+
+/////////////////////////////////////
+// // Register the AppIdentityDbContext for the user database
+// builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+//     options.UseMySQL(builder.Configuration.GetConnectionString("IdentityDbConnection")));
+//
+// // Use AddIdentity with a custom user (AppIdentityUser) and role (IdentityRole)
+// builder.Services.AddIdentity<AppIdentityUser, IdentityRole>()
+//     .AddEntityFrameworkStores<AppIdentityDbContext>()
+//     .AddDefaultTokenProviders();  // Adds default token providers (for reset passwords, etc.)
+//
+// // Optionally configure Identity options (like requiring account confirmation)
+// builder.Services.Configure<IdentityOptions>(options =>
+// {
+//     options.SignIn.RequireConfirmedAccount = false;
+// });
+/////////////////////////////////////
 
 // Auto mapper
 builder.Services.AddAutoMapper(typeof(Program));
@@ -66,33 +89,62 @@ app.MapControllerRoute(
 //     name: "default",
 //     pattern: "{controller=AuctionItem}/{action=CreateAuctionItem}");
 
-using (var serviceScope = app.Services.CreateScope())
-{
-    var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    var roles = new {Admin = "Admin"};
-    
-    if (!await roleManager.RoleExistsAsync(roles.Admin))
-        await roleManager.CreateAsync(new IdentityRole(roles.Admin));
-}
+// using (var serviceScope = app.Services.CreateScope())
+// {
+//     var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+//     var roles = new {Admin = "Admin"};
+//     
+//     if (!await roleManager.RoleExistsAsync(roles.Admin))
+//         await roleManager.CreateAsync(new IdentityRole(roles.Admin));
+// }
+//
+// using (var serviceScope = app.Services.CreateScope())
+// {
+//     var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<AppIdentityUser>>();
+//     
+//     string email = "admin@gmail.com";
+//     string password = "Test1234,";
+//
+//     if (await userManager.FindByEmailAsync(email) == null)
+//     {
+//         var user = new AppIdentityUser();
+//         user.UserName = email;
+//         user.Email = email;
+//         user.EmailConfirmed = true;
+//         
+//         await userManager.CreateAsync(user, password);
+//         
+//         await userManager.AddToRoleAsync(user, "Admin");
+//     }
+// }
 
-using (var serviceScope = app.Services.CreateScope())
-{
-    var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<AppIdentityUser>>();
-    
-    string email = "admin@gmail.com";
-    string password = "Test1234,";
-
-    if (await userManager.FindByEmailAsync(email) == null)
-    {
-        var user = new AppIdentityUser();
-        user.UserName = email;
-        user.Email = email;
-        user.EmailConfirmed = true;
-        
-        await userManager.CreateAsync(user, password);
-        
-        await userManager.AddToRoleAsync(user, "Admin");
-    }
-}
+// using (var serviceScope = app.Services.CreateScope())
+// {
+//     var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+//     var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<AppIdentityUser>>();
+//
+//     var roles = new { Admin = "Admin" };
+//
+//     if (!await roleManager.RoleExistsAsync(roles.Admin))
+//     {
+//         await roleManager.CreateAsync(new IdentityRole(roles.Admin));
+//     }
+//
+//     string email = "admin@gmail.com";
+//     string password = "Test1234,";
+//
+//     if (await userManager.FindByEmailAsync(email) == null)
+//     {
+//         var user = new AppIdentityUser
+//         {
+//             UserName = email,
+//             Email = email,
+//             EmailConfirmed = true
+//         };
+//
+//         await userManager.CreateAsync(user, password);
+//         await userManager.AddToRoleAsync(user, "Admin");
+//     }
+// }
 
 app.Run();
